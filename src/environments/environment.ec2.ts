1e960aaf868e9042f95e79924dac40e45b8550dc
shreyashristi25 <shreyashristi25@gmail.com>
2026-05-15 12:18:22 +0530

    Add EC2 frontend run configuration

---
+++ src/environments/environment.ec2.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://3.105.82.23:8080',
+  authServerUrl: 'http://3.105.82.23:8081',
+  notificationWsUrl: 'http://3.105.82.23:8088/ws/notifications'
+};
